2b33952e3430c985f83d41a5b957925258d4238f
DESKTOP-CGH48O5\laure <laurenkim002@naver.com>
2024-05-30 17:03:01 +0900

    7주차 Day 5

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,55 +1,54 @@
 const conn = require('../mariadb') //db 모듈
 const {StatusCodes} = require('http-status-codes'); //status code 모듈
 
+//(카테고리 별, 신간 여부) 전체 도서 목록 조회
 const allBooks = (req, res) => {
 
-    let {category_id} = req.query;
+    let {category_id, news, limit, currentPage} = req.query;
+    //limit: page 당 도서 수
+    //currentPage: 현재 몇 페이지인지 ex) 1페이지, 2페이지, ...
+    //offset: limit * (currentPage - 1) 
+    let offset = limit * (currentPage - 1);
 
-    if(category_id)
+    let sql = `SELECT * FROM books `;
+    let values = [];
+    if(category_id && news)
     {
-        let sql = `SELECT * FROM books WHERE category_id = ?`;
-        conn.query(sql, category_id,
-            function(err, results){
-                if(err)
-                {
-                    return res.status(StatusCodes.BAD_REQUEST).json(err);
-                }
-
-                if(results.length)
-                {
-                    return res.status(StatusCodes.OK).json(results);
-                }
-                else{
-                    return res.status(StatusCodes.NOT_FOUND).json({
-                        message: "책이 없습니다."
-                    });
-                }
-            }
-        );
+        sql += `WHERE category_id = ? AND publish_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
+        values = [category_id];
     }
-    else{
-        let sql = `SELECT * FROM books`;
-        conn.query(sql,
-            function(err,results)
+    else if(category_id)
+    {
+        sql += `WHERE category_id = ?`;
+        values = [category_id];
+    }
+    else if(news)
+    {
+        sql += `WHERE publish_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
+    }
+
+    sql += ` LIMIT ? OFFSET ?`;
+    values.push(parseInt(limit));
+    values.push(offset);
+        
+    conn.query(sql, values,
+        function(err, results){
+            if(err)
             {
-                if(err)
-                {
-                    return res.status(StatusCodes.BAD_REQUEST).json(err);
-                }
-
-                if(results.length)
-                {
-                    return res.status(StatusCodes.OK).json(results);
-                }
-                else{
-                    return res.status(StatusCodes.NOT_FOUND).json({
-                        message: "책이 없습니다."
-                    });
-                }
+                return res.status(StatusCodes.BAD_REQUEST).json(err);
             }
-        );
-    }
-    
+
+            if(results.length)
+            {
+                return res.status(StatusCodes.OK).json(results);
+            }
+            else{
+                return res.status(StatusCodes.NOT_FOUND).json({
+                    message: "책이 없습니다."
+                });
+            }
+        }
+    );  
     
 };
 
